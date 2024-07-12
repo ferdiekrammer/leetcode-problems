6a8849d08154fadb2ce2cf7c5c17f325c66bc518
ferdiekramme <91197983+ferdiekrammer@users.noreply.github.com>
2024-07-12 19:32:49 +0100

    another attempt

--- 1717-Maximum-score-from-removing-substrings.py
+++ 1717-Maximum-score-from-removing-substrings.py
@@ -21,4 +21,41 @@ else:
         score += y * s.count('ba')
         s = s.replace('ba','')
 score
+s
+# %%
+s = "aabbaaxybbaabb"
+
+score = 0
+stack = []
+print(s)
+
+for i in range(len(s)):
+    
+    if stack[-1:] == ['a'] and s[-(i+1)] == 'b': # ba
+        print(f'removing {stack[-1]}')
+
+        stack.pop()
+        score += y
+    elif stack[-1:] == ['b'] and s[-(i+1)] == 'a': # ab
+        print(f'removing {stack[-1]}')
+        stack.pop()
+        score += x
+    else:
+        stack.append(s[-(i+1)])
+        print(s[-(i+1)])
+    
+print(score)
+print(''.join(reversed(stack)))
+    
+# %%
+while 'ba' in s or 'ab' in s:
+        while 'ab' in s:
+            score += x * s.count('ab')
+            s = s.replace('ab','')
+        score += y * s.count('ba')
+        s = s.replace('ba','')
+score
+# %%
+stack=[]
+stack[-1:][0]
 # %%
